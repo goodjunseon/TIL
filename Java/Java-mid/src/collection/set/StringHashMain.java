4d266fef20ec59e6a55d28173d96cc26b56b8239
goodjunseon <pzs2001926@gmail.com>
2025-02-24 19:14:23 +0900

    25-02-24

--- Java/Java-mid/src/collection/set/StringHashMain.java
+++ Java/Java-mid/src/collection/set/StringHashMain.java
@@ -4,31 +4,31 @@ public class StringHashMain {
     static final int CAPACITY = 10;
 
     public static void main(String[] args) {
-        //char
 
         char charA = 'A';
         char charB = 'B';
-        System.out.println(charA + " = " + (int)charA);
-        System.out.println(charB + " = " + (int)charB);
 
-        //hashCode
-        System.out.println("hashCode(A) = " + hashCode("A"));
-        System.out.println("hashCode(B) = " + hashCode("B"));
-        System.out.println("hashCode(AB) = " + hashCode("AB"));ㄴㅇ
-        System.out.println("hashCode(BA) = " + hashCode("BA"));
+        System.out.println("charA = " + (int) charA);
+        System.out.println("charA = " + (int) charA);
 
-        //hashIndex
-        System.out.println("hashIndex(A) = " + hashIndex(hashCode("A")));
-        System.out.println("hashIndex(B) = " + hashIndex(hashCode("B")));
-        System.out.println("hashIndex(AB) = " + hashIndex(hashCode("AB")));
-        System.out.println("hashIndex(BA) = " + hashIndex(hashCode("BA")));
+        // hashCode
+        System.out.println("hashCode(\"A\") = " + hashCode("A"));
+        System.out.println("hashCode(\"B\") = " + hashCode("B"));
+        System.out.println("hashCode(\"AB\") = " + hashCode("AB"));
+
+        // hashIndex
+        System.out.println("hashIndex(\"A\") = " + hashIndex(hashCode("A")));
+        System.out.println("hashIndex(\"B\") = " + hashIndex(hashCode("B")));
+        System.out.println("hashIndex(\"AB\") = " + hashIndex(hashCode("AB")));
+
     }
 
     static int hashCode(String str) {
         char[] charArray = str.toCharArray();
         int sum = 0;
+
         for (char c : charArray) {
-            sum+= c;
+            sum += c;
         }
         return sum;
     }
@@ -36,4 +36,7 @@ public class StringHashMain {
     static int hashIndex(int value) {
         return value % CAPACITY;
     }
+
+
+
 }
